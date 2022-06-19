b712806504f65e6a9ad0ab59fffbfbd014373ea7
puranchandralohar <clpuran95@gmail.com>
2022-06-19 13:25:22 +0530

    While loop Added

--- question3.js
+++ question3.js
@@ -1,9 +1,17 @@
 //Write a program to read the first and last digit of a number.
 
 let number = parseInt(process.argv[2]);
-let firstDigit = Math.floor(number/100);
+// let firstDigit = Math.floor(number / 100);
 
-let lastDigit = number%10;
+// let lastDigit = number % 10;
 
-console.log("First Digit ="+firstDigit+ ", Last Digit = " +lastDigit);
+// console.log("First Digit =" + firstDigit + ", Last Digit = " + lastDigit);
 
+// While loop
+let firstDigit, lastDigit;
+lastDigit = number % 10;
+while (number >= 10) {
+  firstDigit = Math.floor(number / 10);
+  number /= 10;
+}
+console.log(firstDigit, lastDigit);
